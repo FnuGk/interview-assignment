322aad554b2703b7d978e92ff6be77a0c4cf1bc1
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2017-09-28 21:46:20 +0200

    an error in run.sh made it not work if the out folder did not exist

--- go-solution/run.sh
+++ go-solution/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rm -f test.db && cat ../sqldump.sql | sqlite3 test.db && rm -r out && go run main.go
+rm -f test.db && cat ../sqldump.sql | sqlite3 test.db && rm -fr out && go run main.go
