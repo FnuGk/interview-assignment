6ea3566630d2742ee7581890380e452d2db45745
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2017-09-28 17:04:14 +0200

    db: implemented UserDB.DeleteByID

--- go-solution/db/user.go
+++ go-solution/db/user.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 
 	"github.com/fnugk/interview-assignment/go-solution/model"
+	"github.com/pkg/errors"
 )
 
 // IUserDB exposes all functionality of a UserDB
@@ -21,4 +22,28 @@ type UserDB struct {
 // NewUserDB creates a new UserDB
 func NewUserDB(db *DB) *UserDB {
 	return &UserDB{db}
+}
+
+// DeleteByID deletes the user by the given id
+func (db *UserDB) DeleteByID(ctx context.Context, tx *sql.Tx, id string) error {
+	q := `
+		DELETE FROM useres
+		WHERE id=?
+	`
+
+	res, err := tx.ExecContext(ctx, q, id)
+	if err != nil {
+		return errors.Wrapf(err, "could not delete users with id %#v", id)
+	}
+
+	// though unique id is not enforced by the db, this assumes unique id
+	n, err := res.RowsAffected()
+	if err != nil {
+		return errors.Wrapf(err, "could not get RowsAffected when deleteing by id %#v", id)
+	}
+
+	if n != 1 {
+		return errors.Errorf("expected to delete exactly 1 row instead deleted %d rows", n)
+	}
+	return nil
 }
